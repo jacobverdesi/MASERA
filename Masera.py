126252e81cf30dce1e6f8ad0f519eac398f687c4
Jacob's Laptop <jxv3386@rit.edu>
2020-02-21 12:23:26 -0500

    Init

---
+++ Masera.py
@@ -0,0 +1,19 @@
+import threading
+
+
+
+def Agent(num):
+    print("Agent #",num)
+    return
+
+
+
+def main():
+    agents=[]
+    for i in range(5):
+        t = threading.Thread(target=Agent(i))
+        agents.append(t)
+        t.start()
+
+if __name__ == '__main__':
+    main()
