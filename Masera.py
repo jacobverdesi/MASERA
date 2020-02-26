8e1bec4e817cf02a18819ae0e3a04441e6f5d3d8
Jacob's Laptop <jxv3386@rit.edu>
2020-02-26 16:42:45 -0500

    push

--- Masera.py
+++ Masera.py
@@ -7,7 +7,6 @@ from matplotlib.patches import Circle
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from itertools import combinations
-
 import numpy as np
 import logging
 logging.basicConfig(level=logging.DEBUG,format='(%(threadName)-10s) %(message)s',)
@@ -16,29 +15,39 @@ class Agent(threading.Thread):
     def __init__(self,id,type,prodTime,initBudget,initFood,initFoodPrice,riskFactor,nbrRadius,x, y, vx, vy, radius):
         super().__init__()
         logging.debug("Initilizing Agent #%s", id)
-        self.day=0
+        self.day = 0
         self.type = type
         self.id = id
-        self.prodTime=prodTime
-        self.money=initBudget
-        self.food=initFood
-        self.foodPrice=initFoodPrice
-        self.risk=riskFactor
-        self.nbrRadius=nbrRadius
+        self.prodTime = prodTime
+        self.money = initBudget
+        self.food = initFood
+        self.foodPrice = initFoodPrice
+        self.risk = riskFactor
+        self.nbrRadius = nbrRadius
         self.x, self.y, self.vx, self.vy = x, y, vx, vy
         self.r = np.array((x, y))
         self.v = np.array((vx, vy))
         self.radius = radius
+        self.wait = False
+        self.currentDay=0
+        self.maxDays=0
         #edge color ,'edgecolor': types[(type+1)%len(types)]
         self.style={'linewidth': 2, 'facecolor': AGENTDATA.get(type)[2]}
 
     def run(self):
-
         logging.debug("Starting Agent %s",self.id)
-
+        while self.day<self.maxDays:
+            self.runDay()
+            while self.wait:
+                time.sleep(1)
+        logging.debug("Ending Agent %s",self.id)
     def runDay(self):
-        time.sleep(random.random()*5)
-        logging.debug("Agent %s is done for the day",self.id)
+        lenght = random.random() * 5
+        time.sleep(lenght)
+        logging.debug("Agent %s slept for %s", self.id, lenght)
+        self.day+=1
+        self.wait=True
+
     def clone(self):
         return Agent(self.id,self.type,self.prodTime,self.money,self.food,self.foodPrice,self.risk,self.nbrRadius,self.x,self.y,self.vx,self.vy,self.radius)
     def overlaps(self, other):
@@ -79,8 +88,9 @@ class MASERA:
    •	 size of the 2D Environment.
     """
 
-    def __init__(self,initBudget,initFood,initFoodPrice,riskFactor,nbrRadius,enviromentSize):
+    def __init__(self,days,initBudget,initFood,initFoodPrice,riskFactor,nbrRadius,enviromentSize):
         self.agents = []
+        self.maxDays=days
         id = 0
         self.enviromentSize=np.array((enviromentSize[0],enviromentSize[1]))
         radius=min(enviromentSize)/50
@@ -94,12 +104,25 @@ class MASERA:
                 agent=Agent(id, key,prodTime,initBudget,initFood,initFoodPrice,random.uniform(riskFactor[0],riskFactor[1]),nbrRadius,x, y, vx, vy,radius)
                 agent.setName(key+":"+str(id))
                 self.agents.append(agent)
-    print("test")
-    def initSimu(self):
-        print("Initializing Simul")
-        self.circles = []
+        self.initSimulation()
+        self.anim = animation.FuncAnimation(self.fig, self.runDay, frames=days,interval=5000, blit=True,repeat=False)
+
+        self.save()
+    def initSimulation(self):
+        print("Initializing Simulation")
         print(self.agents)
+        self.fig, self.ax = plt.subplots()
+        for s in ['top', 'bottom', 'left', 'right']:
+            self.ax.spines[s].set_linewidth(2)
+        self.ax.set_aspect('equal', 'box')
+        self.ax.set_xlim(0, self.enviromentSize[0])
+        self.ax.set_ylim(0, self.enviromentSize[1])
+        self.ax.xaxis.set_ticks([])
+        self.ax.yaxis.set_ticks([])
+        self.circles = []
         for agent in self.agents:
+            agent.maxDays=self.maxDays
+            agent.start()
             circle=agent.draw()
             self.ax.add_patch(circle)
             self.circles.append(circle)
@@ -107,41 +130,20 @@ class MASERA:
     def runDay(self,i):
         print("Day:",i)
         for i, p in enumerate(self.agents):
-            p.runDay()
+            p.wait=False
             self.circles[i].center = (p.x,p.y)
-
-        #self.handle_collisions()
-        # copy=[]
-        # for agent in self.agents:
-        #     copy.append(agent.clone())
-        # self.agents.clear()
-        # self.agents=copy
         return self.circles
-    def do_animation(self, save=False):
-        fig, self.ax = plt.subplots()
-        for s in ['top','bottom','left','right']:
-            self.ax.spines[s].set_linewidth(2)
-        self.ax.set_aspect('equal', 'box')
-        self.ax.set_xlim(0, self.enviromentSize[0])
-        self.ax.set_ylim(0, self.enviromentSize[1])
-        self.ax.xaxis.set_ticks([])
-        self.ax.yaxis.set_ticks([])
-
-        for agent in self.agents:
-            agent.start()
-
-        anim = animation.FuncAnimation(fig, self.runDay, init_func=self.initSimu,frames=800, interval=2, blit=True)
+    def save(self,save=False):
         if save:
             Writer = animation.writers['imagemagick']
             writer = Writer(fps=100, bitrate=1800)
-            anim.save('Masera.gif', writer=writer)
+            self.anim.save('Masera.gif', writer=writer)
         else:
             plt.show()
 
 def main():
     global AGENTDATA
     AGENTDATA={"Farmer": (2, 1, "seagreen"), "Miller": (1, 2, "orange")}
-    sim = MASERA(1000,5,1,(0.0,0.5),100,(10,10))
-    sim.do_animation()
+    sim = MASERA(10,1000,5,1,(0.0,0.5),100,(10,10))
 if __name__ == '__main__':
     main()
